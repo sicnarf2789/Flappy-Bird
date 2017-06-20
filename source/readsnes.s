e3652da43f94ef857ccda5764e08d3c9f6d95840
Gil Francis Abinal <gfabinal@gmail.com>
2017-06-20 12:45:53 -0600

    Update readsnes.s

--- source/readsnes.s
+++ source/readsnes.s
@@ -2,83 +2,124 @@
 	contains the draw functions
 */
 
-.globl drawImage
-drawImage:								//r0, r1 = starting x, y positions
-	push {r5, r6, r7, r8, r9, r10, lr}		//r2, r3 = image width and height
-	mov	r5,	r0							//r4 = address of image
-	mov	r6,	r1
-	mov     r7,     r4
-	mov	r8,	r2
-        mov     r10,    r5
-	mov	r9,	r3
-	add     r8,     r5
-	add     r9,     r6
+.globl InitGPIO
+
+InitGPIO:
+
+	ldr	r0, =0x3F200000			//Initialize Latch
+	mov	r1,	#27
+	mov r2, #0b001
+	bl	init_GPIO
 	
-drawImageLoop:
-	mov	r0,	r5							//passing x for ro which is used by the Draw pixel function 
-	mov	r1,	r6							//passing y for r1 which is used by the Draw pixel formula 
+	ldr	r0, =0x3F200004			//Initialize Data
+	mov	r1,	#0
+	mov r2, #0b000
+	bl	init_GPIO
 	
-	ldrh r2, [r7],#2					//setting pixel color by loading it from the data section. We load hald word
+	ldr	r0, =0x3F200004			//Initializa Clock
+	mov	r1,	#3
+	mov r2, #0b001
+	bl	init_GPIO
 
-	bl	DrawPixel
-	add	r5,	#1			//increment x position
-	cmp	r5,	r8			//compare with image with
-	blt	drawImageLoop
-	//sub r5, r8			//reset x
-        mov     r5,     r10	
-        add	r6,	#1			//increment Y
-	cmp	r6,	r9			//compare y with image height
-	blt	drawImageLoop
-	pop {r5, r6, r7, r8, r9, r10, pc}
-    
 
-/* Draw Pixel
- *  r0 - x
- *  r1 - y
- *  r2 - color
- */
 
-DrawPixel:
-	push {r4}
-	offset	.req	r4
-
-	// offset = (y * 1024) + x = x + (y << 10)
-	add offset, r0, r1, lsl #10
+init_GPIO:						//param: r0 = address, r1 = GPIO line, r2 = func select value
+	push {r4, r5}				//no return values
+	ldr r4, [r0]				//intializes a single GPIO line
 	
-	// offset *= 2 (for 16 bits per pixel = 2 bytes per pixel)
-	lsl offset, #1
-
-	// store the colour (half word) at framebuffer pointer + offset
-
-	ldr r0, =FrameBufferPointer
-	ldr r0, [r0]
-	strh r2, [r0, offset]
-
-	pop {r4}
-	bx lr
-
-
-.globl clearScreen
-clearScreen:
-	mov	r4,	#0			//x value
-	mov	r5,	#0			//Y value
-	mov	r6,	#0			//black color
-	ldr	r7,	=1023			//Width of screen
-	ldr	r8,	=767			//Height of the screen
+	mov r5, #0b111
+	bic r4, r5, lsl r1
 	
-Looping:
-	mov	r0,	r4			//Setting x 
-	mov	r1,	r5			//Setting y
-	mov	r2,	r6			//setting pixel color
-	push {lr}
-	bl	DrawPixel
-	pop {lr}
-	add	r4,	#1			//increment x by 1
-	cmp	r4,	r7			//compare with width
-	blt	Looping
-	mov	r4,	#0			//reset x
-	add	r5,	#1			//increment Y by 1
-	cmp	r5,	r8			//compare with height
-	blt	Looping
+	mov r5, r2
+	orr r4, r5, lsl r1
 	
-	mov	pc,	lr			//return
+	str r4, [r0]
+	pop {r4, r5}
+	mov pc, lr
+
+.globl readSNES	
+readSNES:						//reads controller input, no param
+	push {r4, r5, lr}			//returns the 16bit binary number representing input in r2
+	mov	r4, #0					//initialize buttons to 0
+	mov	r5, #0					//initialize counter to 0
+	
+	ldr r2, =0x186A0
+	bl wait
+
+	mov r1, #1
+	mov r0, #9
+	bl write_Pin				//set latch to 1
+
+	mov r1, #1
+	mov r0, #11
+	bl write_Pin				//set clock to 1
+
+	mov r2, #12 
+	bl wait						//wait for 12 microseconds
+	
+	mov r1, #0
+	mov r0, #9
+	bl write_Pin				//set latch to 0
+
+pulse:
+	mov r2, #6
+	bl wait						//wait for 6 microseconds
+	
+	mov r1, #0
+	mov r0, #11
+	bl write_Pin				//set clock to 0
+	
+	mov r2, #6
+	bl wait						//wait for 6 microseconds
+
+	mov r0, #10
+	bl read_Pin					//read data
+
+	mov r6, #2
+	mul r4, r6					//shift buttons left then add read pin value
+	add r4, r0
+
+	mov r1, #1
+	mov r0, #11
+	bl write_Pin				//set clock to 1
+
+	add r5, #1					//loop guard, stops once r5 = 16
+	cmp r5, #16
+	blt pulse
+	
+	mov r2, r4					//return buttons to r2 register
+	pop {r4, r5, lr}
+	mov	pc, lr
+	
+write_Pin:						//writes a bit(1 or 0) to a pin
+	ldr r2, =0x3F200000			//param: r0 = pin number, r1 = bit to write
+	mov r3, #1
+	lsl r3, r0
+	teq r1, #0
+	streq r3, [r2, #40]
+	strne r3, [r2, #28]
+	mov	pc, lr
+	
+read_Pin:						//reads a bit(1 or 0) from a pin
+	ldr	r2, =0x3F200000			//param: r0 = pin number
+	ldr	r1, [r2, #52]			//return: returns the read bit in r0
+	mov	r3, #1
+	lsl	r3, r0
+	and r1, r3
+	teq	r1, #0
+	moveq r0, #0
+	movne r0, #1
+	mov	pc, lr
+	
+wait:							//waits for selected amount of time
+	ldr	r0, =0x3F003004			//param: r2 = # of nanoseconds waited
+	ldr	r1, [r0]
+	add	r1, r2
+waitLoop:
+	ldr	r3, [r0]
+	cmp	r1, r3
+	bhi	waitLoop
+	mov	pc, lr
+	
+	
+
